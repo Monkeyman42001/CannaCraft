b24d7ad7e3a1bd1b3a47eb58f27c402e91cd9257
Monkeyman42001 <nmparks420@outlook.com>
2026-02-12 09:23:06 -0500

    added vape cart textures

---
+++ src/main/java/net/monkeyman42001/cannacraft/item/VapeCartriage.java
@@ -0,0 +1,9 @@
+package net.monkeyman42001.cannacraft.item;
+
+import net.minecraft.world.item.Item;
+
+public class VapeCartriage extends Item {
+    public VapeCartriage(Properties properties) {
+        super(properties);
+    }
+}
